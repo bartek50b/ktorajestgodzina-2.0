bc003628974e9f5383d3e669c2d436fb3fb59cd8
bartek50b <61241666+bartek50b@users.noreply.github.com>
2025-11-15 20:10:56 +0100

    Added interactive world map.
    You can use map to change timezones

---
+++ map.js
@@ -0,0 +1,51 @@
+import { regionToTimezone } from "./data.js";
+import { changeTimezone } from "./script.js";
+fetch("media/svg/timezones.svg")
+  .then((response) => response.text())
+  .then((svg) => {
+    document.getElementById("map").innerHTML = svg;
+    const regions = document.getElementsByClassName("o");
+
+    let selectedRegion = localStorage.getItem("selectedRegion")
+      ? localStorage.getItem("selectedRegion")
+      : "pl";
+    document.getElementById(selectedRegion).classList.add("chosen");
+
+    Array.from(regions).forEach((region) => {
+      region.addEventListener("mouseover", () => {
+        const timezone = regionToTimezone[region.id];
+        if (!timezone) return;
+        const sameTimezoneRegions = document.getElementsByClassName(timezone);
+
+        Array.from(sameTimezoneRegions).forEach((sameTimezoneRegion) => {
+          if (sameTimezoneRegion.id != region.id) {
+            sameTimezoneRegion.classList.add("same-timezone");
+          } else {
+            sameTimezoneRegion.classList.add("hovered");
+          }
+        });
+      });
+
+      region.addEventListener("mouseout", () => {
+        const timezone = regionToTimezone[region.id];
+        if (!timezone) return;
+        const sameTimezoneRegions = document.getElementsByClassName(timezone);
+
+        Array.from(sameTimezoneRegions).forEach((sameTimezoneRegion) => {
+          if (sameTimezoneRegion.id != region.id) {
+            sameTimezoneRegion.classList.remove("same-timezone");
+          } else {
+            sameTimezoneRegion.classList.remove("hovered");
+          }
+        });
+      });
+
+      region.addEventListener("click", () => {
+        const timezone = regionToTimezone[region.id];
+        changeTimezone(String(timezone));
+        Array.from(regions).forEach((r) => r.classList.remove("chosen"));
+        region.classList.add("chosen");
+        localStorage.setItem("selectedRegion", region.id);
+      });
+    });
+  });
--- script.js
+++ script.js
@@ -1,116 +1,56 @@
-const skyColors = [
-  "rgb(2, 7, 24)",
-  "rgb(3, 10, 31)",
-  "rgb(5, 13, 39)",
-  "rgb(7, 16, 48)",
-  "rgb(9, 21, 60)",
-  "rgb(20, 38, 73)",
-  "rgb(39, 74, 114)",
-  "rgb(60, 109, 154)",
-  "rgb(79, 134, 181)",
-  "rgb(93, 147, 192)",
-  "rgb(107, 160, 203)",
-  "rgb(119, 169, 209)",
-  "rgb(130, 178, 215)",
-  "rgb(127, 175, 220)",
-  "rgb(123, 172, 223)",
-  "rgb(118, 168, 224)",
-  "rgb(106, 156, 219)",
-  "rgb(91, 139, 207)",
-  "rgb(71, 111, 180)",
-  "rgb(53, 78, 131)",
-  "rgb(40, 52, 90)",
-  "rgb(27, 34, 58)",
-  "rgb(16, 20, 38)",
-  "rgb(6, 8, 20)",
-];
+import { skyColors, sunMoonColors, sunPositions } from "./data.js";
 
-const sunMoonColors = [
-  "rgb(191, 201, 255)",
-  "rgb(195, 204, 255)",
-  "rgb(199, 208, 255)",
-  "rgb(203, 211, 255)",
-  "rgb(208, 215, 255)",
-  "rgb(247, 194, 122)",
-  "rgb(255, 193, 90)",
-  "rgb(255, 208, 102)",
-  "rgb(255, 221, 115)",
-  "rgb(255, 227, 122)",
-  "rgb(255, 232, 129)",
-  "rgb(255, 235, 133)",
-  "rgb(255, 240, 140)",
-  "rgb(255, 233, 128)",
-  "rgb(255, 223, 114)",
-  "rgb(255, 210, 97)",
-  "rgb(255, 193, 82)",
-  "rgb(255, 173, 71)",
-  "rgb(255, 154, 74)",
-  "rgb(231, 197, 255)",
-  "rgb(217, 201, 255)",
-  "rgb(207, 205, 255)",
-  "rgb(197, 209, 255)",
-  "rgb(186, 198, 255)",
-];
+let timeDiff = 0;
+let skyTime = new Date();
 
-const sunPositions = [
-  [95, 90],
-  [92, 88],
-  [88, 85],
-  [84, 82],
-  [80, 78],
-  [76, 70],
-  [72, 60],
-  [68, 45],
-  [64, 30],
-  [60, 18],
-  [55, 8],
-  [52, 2],
-  [50, 0],
-  [48, 2],
-  [44, 8],
-  [40, 18],
-  [36, 30],
-  [32, 45],
-  [28, 60],
-  [24, 70],
-  [20, 78],
-  [14, 82],
-  [8, 85],
-  [4, 90],
-];
-
-function updateClock() {
+let updateClock = function updateClock() {
   const time = new Date();
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
+  const adjustedTime = new Date(time.getTime() + timeDiff);
+  const hours = adjustedTime.getUTCHours();
+  const minutes = adjustedTime.getUTCMinutes();
+  const seconds = adjustedTime.getUTCSeconds();
+
   const timeToString =
     String(hours).padStart(2, "0") + ":" + String(minutes).padStart(2, "0");
   const secondsToString = String(seconds).padStart(2, "0");
   document.getElementById("clock").innerHTML = timeToString;
   document.getElementById("clock-seconds").innerHTML = secondsToString;
-  changeColoursBasedOnTime(hours, minutes);
-}
+};
 
-function changeColoursBasedOnTime(hour, minute) {
-  fraction = minute / 60;
-  const interpolatedSkyColor = CalculateColorInterpolation(
+function changeColoursBasedOnSkyTime() {
+  const hour = skyTime.getUTCHours();
+  const minute = skyTime.getUTCMinutes();
+  const fraction = minute / 60;
+  const CalculatedInterpolatedSkyColor = CalculateColorInterpolation(
     skyColors[hour],
     skyColors[(hour + 1) % 24],
     fraction
   );
-  const interpolatedSunMoonColor = CalculateColorInterpolation(
+  const CalculatedinterpolatedSunMoonColor = CalculateColorInterpolation(
     sunMoonColors[hour],
     sunMoonColors[(hour + 1) % 24],
     fraction
   );
-  const sunPosition = CalculateSunPosition(
+  const CalculatedsunPosition = CalculateSunPosition(
     sunPositions[hour],
     sunPositions[(hour + 1) % 24],
     fraction
   );
 
-  document.body.style.background = `radial-gradient(${sunPosition} ,${interpolatedSunMoonColor}, ${interpolatedSkyColor})`;
+  document.body.style.background = `radial-gradient(${CalculatedsunPosition} ,${CalculatedinterpolatedSunMoonColor}, ${CalculatedInterpolatedSkyColor})`;
+}
+
+function adjustSkyTime(timeActual) {
+  const diff = skyTime.getTime() - timeActual.getTime();
+
+  if (Math.abs(diff) < 600000) {
+    return;
+  }
+  if (diff < 0) {
+    skyTime = new Date(skyTime.getTime() + 720000);
+  } else {
+    skyTime = new Date(skyTime.getTime() - 720000);
+  }
 }
 
 function CalculateColorInterpolation(color1, color2, fraction) {
@@ -124,11 +64,18 @@ function CalculateColorInterpolation(color1, color2, fraction) {
 }
 
 function CalculateSunPosition(pos1, pos2, fraction) {
-  p1 = Math.round((pos2[0] - pos1[0]) * fraction + pos1[0]);
-  p2 = Math.round((pos2[1] - pos1[1]) * fraction + pos1[1]);
+  const p1 = Math.round((pos2[0] - pos1[0]) * fraction + pos1[0]);
+  const p2 = Math.round((pos2[1] - pos1[1]) * fraction + pos1[1]);
 
   return "at " + p1 + "% " + p2 + "%";
 }
+
+setInterval(() => {
+  const now = new Date();
+  const adjustedNow = new Date(now.getTime() + timeDiff);
+  adjustSkyTime(adjustedNow);
+  changeColoursBasedOnSkyTime();
+}, 100);
 
 updateClock();
 setInterval(updateClock, 1000);
@@ -160,3 +107,17 @@ buttons.forEach((button) => {
     }, 1500);
   });
 });
+
+export function changeTimezone(timezone) {
+  const plusMinus = timezone[4] == "p" ? 1 : -1;
+  if (timezone.length == 6) {
+    timeDiff = plusMinus * 3600000 * timezone[5];
+  } else if (timezone[7] == 3) {
+    timeDiff = plusMinus * 3600000 * (timezone[5] + 0.5);
+  } else if (timezone[7] == 4) {
+    ////0.45
+    timeDiff = plusMinus * 3600000 * (timezone[5] + 0.75);
+  } else {
+    timeDiff = 0;
+  }
+}
